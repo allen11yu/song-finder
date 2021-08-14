ec95b133f456c11fe23029119f43377844e3b2ea
Allen Yu <yu.junhong02@gmail.com>
2021-08-14 15:07:29 -0700

    added documentation

--- api_key.js
+++ api_key.js
@@ -5,17 +5,17 @@ var miapi = {
   URL: "https://music-identify.p.rapidapi.com/identify",
   HEADERS: {
     // You can leave out "content-type"
-		"x-rapidapi-key": "Paste key here",
-		"x-rapidapi-host": "Paste host here"
-	}
+    "x-rapidapi-key": "Paste key here",
+    "x-rapidapi-host": "Paste host here"
+  }
 };
 
 /** The API key for Shazam API. */
 var shazamapi = {
-	URL: "https://shazam.p.rapidapi.com/search?",
-	HEADERS: {
+  URL: "https://shazam.p.rapidapi.com/search?",
+  HEADERS: {
     // You can leave out "content-type"
-		"x-rapidapi-key": "Paste key here",
-		"x-rapidapi-host": "Paste host here"
-	}
+    "x-rapidapi-key": "Paste key here",
+    "x-rapidapi-host": "Paste host here"
+  }
 }
